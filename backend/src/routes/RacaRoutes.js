c4ebcde2e088718266dd7a4adad4669103fb7008
l4ger007 <thelufaoo@gmail.com>
2025-12-09 21:25:07 -0300

    fix: correcao no cadastro de animais e busca de raca por id

--- backend/src/routes/RacaRoutes.js
+++ backend/src/routes/RacaRoutes.js
@@ -4,6 +4,7 @@ import RacaController from "../controllers/RacaController.js";
 const router = Router();
 router.get("/racas", RacaController.listar);
 router.post("/racas", RacaController.criar);
+router.get("/racas/:id", RacaController.buscarPorId);
 router.put("/racas/:id", RacaController.atualizar);
 router.delete("/racas/:id", RacaController.excluir);
 
