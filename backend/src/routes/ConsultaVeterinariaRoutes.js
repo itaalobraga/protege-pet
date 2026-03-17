caa46c34ac1e0e12982f289559eca7d5a0cc95a0
itaalobraga <itojk2@hotmail.com>
2026-03-17 19:04:31 -0300

    feat: add authentication middleware to ConsultaVeterinariaRoutes

--- backend/src/routes/ConsultaVeterinariaRoutes.js
+++ backend/src/routes/ConsultaVeterinariaRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import ConsultaVeterinariaController from "../controllers/ConsultaVeterinariaController.js";
+import { authJwt } from "../middlewares/authJwt.js";
 
 const router = express.Router();
+
+router.use(authJwt);
 
 router.get("/consultas-veterinarias", ConsultaVeterinariaController.listar);
 router.get("/consultas-veterinarias/:id", ConsultaVeterinariaController.buscarPorId);
